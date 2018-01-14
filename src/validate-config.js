3a211ccd4a48e8535965273db854d95f862dd0e1
Steel Brain <de.steelbrain@gmail.com>
2018-01-15 00:26:31 +0500

    :new: Add config-validator

---
+++ src/validate-config.js
@@ -0,0 +1,46 @@
+// @flow
+
+import invariant from 'assert'
+import type { Config } from './types'
+
+const configTogglesAndCallbackPairs = [
+  ['allowSFTP', 'shouldAllowSFTP'],
+  ['allowShell', 'shouldAllowShell'],
+  ['allowExec', 'shouldAllowExec'],
+  ['allowWrites', 'shouldAllowWrite'],
+  ['allowEscapingRoot', 'shouldAllowEscapingRoot'],
+]
+const configTypes = [
+  ['server', ['undefined', 'object']],
+  ['authenticate', ['function']],
+  ['userShell', ['undefined', 'string']],
+  ['getUserShell', ['undefined', 'function']],
+  ['getUserSystemUser', ['undefined', 'function']],
+  ['getUserRootDirectory', ['function']],
+]
+
+export default function validateConfig(config: Config): void {
+  invariant(config && typeof config === 'object', 'config must be an object')
+
+  configTypes.forEach(([name, allowedTypes]) => {
+    invariant(
+      allowedTypes.includes(typeof config[name]) || (typeof config[name] === 'object' && !config[name]),
+      `config.${name} must be one of ${allowedTypes.join(', ')}`,
+    )
+  })
+
+  configTogglesAndCallbackPairs.forEach(([toggle, callback]) => {
+    invariant(['undefined', 'boolean'].includes(typeof config[toggle]), `config.${toggle} must be undefined or boolean`)
+    if (!config[toggle]) {
+      invariant(
+        typeof config[callback] === 'undefined',
+        `config.${callback} must not be specified when config.${toggle} is false`,
+      )
+    } else {
+      invariant(
+        ['undefined', 'function'].includes(typeof config[callback]),
+        `config.${callback} must be undefined or function when config.${toggle} is true`,
+      )
+    }
+  })
+}
